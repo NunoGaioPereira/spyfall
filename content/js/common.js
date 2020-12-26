355a0254ccbae8599fb3c25663cbc2cf2bcaa7fd
Nuno Pereira <nunocgpereira@gmail.com>
2020-12-26 20:33:20 +0000

    Isolate common js, general refactor and style

---
+++ content/js/common.js
@@ -0,0 +1,45 @@
+const app = () => {
+
+    window.addEventListener('load', function(){
+        const loader = document.querySelector('.loader');
+        loader.className += ' hidden';
+    });
+
+    const bd = document.getElementsByTagName('body')[0];
+
+    const start = document.getElementById('start');
+    const modal = document.querySelector('.modal');
+    const cross = document.querySelector('.cross');
+    const app_body = document.querySelector('.app');
+
+    const logo = document.querySelector('.logo');
+
+    /* --- Theme toggle --- */
+    // Apply cached theme on reload
+    const theme_check = document.querySelector('.themeCheck');
+    const body = document.body;
+    const isDark = localStorage.getItem('isDark');
+
+    if (isDark == 'true') { 
+        body.classList.replace('light', 'dark');
+        theme_check.checked = true;
+        // bd.setAttribute("style", "--track-outline: #9298aa");
+        logo.src = "./includes/imgs/logo_dark.png";
+    }
+    theme_check.addEventListener('click', () => {
+        if (theme_check.checked) {
+            body.classList.replace('light', 'dark');
+            localStorage.setItem('isDark', true);
+            bd.setAttribute("style", "--track-outline: #9298aa");
+            logo.src = "./includes/imgs/logo_dark.png";
+        }
+        else {
+            body.classList.replace('dark', 'light');
+            localStorage.setItem('isDark', false);
+            bd.setAttribute("style", "--track-outline: #f2f2f2");
+            logo.src = "./includes/imgs/logo.png";
+        }
+    });
+};
+
+app();
--- content/js/home.js
+++ content/js/home.js
@@ -1,12 +1,4 @@
-const app = () => {
-
-    window.addEventListener('load', function(){
-        const loader = document.querySelector('.loader');
-        loader.className += ' hidden';
-    });
-
-
-
+// const app = () => {
     const name_modal = document.querySelector('.name-modal');
     const cover = document.querySelector('#cover');
     const join_button = document.querySelector('#join');
@@ -33,48 +25,6 @@ const app = () => {
             // return false;
         }
     }
+// };
 
-
-
-
-
-
-
-    const bd = document.getElementsByTagName('body')[0];
-
-    const start = document.getElementById('start');
-    const modal = document.querySelector('.modal');
-    const cross = document.querySelector('.cross');
-    const app_body = document.querySelector('.app');
-
-    const logo = document.querySelector('.logo');
-
-    /* --- Theme toggle --- */
-    // Apply cached theme on reload
-    const theme_check = document.querySelector('.themeCheck');
-    const body = document.body;
-    const isDark = localStorage.getItem('isDark');
-
-    if (isDark == 'true') { 
-        body.classList.replace('light', 'dark');
-        theme_check.checked = true;
-        // bd.setAttribute("style", "--track-outline: #9298aa");
-        logo.src = "./includes/imgs/logo_dark.png";
-    }
-    theme_check.addEventListener('click', () => {
-        if (theme_check.checked) {
-            body.classList.replace('light', 'dark');
-            localStorage.setItem('isDark', true);
-            bd.setAttribute("style", "--track-outline: #9298aa");
-            logo.src = "./includes/imgs/logo_dark.png";
-        }
-        else {
-            body.classList.replace('dark', 'light');
-            localStorage.setItem('isDark', false);
-            bd.setAttribute("style", "--track-outline: #f2f2f2");
-            logo.src = "./includes/imgs/logo.png";
-        }
-    });
-};
-
-app();
+// app();
